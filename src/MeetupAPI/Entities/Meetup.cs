e447d311c7b5d06eb370f12b99a0c6d2b7f5554b
sayan95 <sharthakmallik@gmail.com>
2022-04-06 22:16:51 +0530

    First commit

---
+++ src/MeetupAPI/Entities/Meetup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetupAPI.Entities
+{
+    public class Meetup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Organizer { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPrivate { get; set; }
+
+        public Location Location { get; set; }
+
+        public List<Lecture> Lectures { get; set; }
+    }
+}
